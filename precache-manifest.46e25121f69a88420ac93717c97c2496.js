e98067a7befbe1437e0222c4ffb71aecf211bd03
Solaiyurmani <Aravinth.Solaiyurmani@tesco.com>
2020-01-24 07:10:52 +0000

    First Build Version

---
+++ precache-manifest.46e25121f69a88420ac93717c97c2496.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3e112eef5ae09069a75f9fba1f5f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcaf0f48104cb66c88f3",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6261d2bab9a5664eae95",
+    "url": "/static/js/2.1356adc2.chunk.js"
+  },
+  {
+    "revision": "84f5adf97a6a466dd50e6b2b7291bba3",
+    "url": "/static/js/2.1356adc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcaf0f48104cb66c88f3",
+    "url": "/static/js/main.e0439a8b.chunk.js"
+  },
+  {
+    "revision": "9af90a3bdb44f1b1746a",
+    "url": "/static/js/runtime-main.fb18d836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
